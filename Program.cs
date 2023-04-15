8c72ee989913147f13dfc5b6c345ffccaec6b46d
Dmitiy Kochkin <dmm_dmm_kdv@example.com>
2023-04-15 12:34:36 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int num_a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num_b = int.Parse(Console.ReadLine()!);
+if (num_a > num_b) {
+    Console.WriteLine("Наибольшее число = " + num_a);
+    Console.WriteLine("Наименьшее число = " + num_b);
+    }
+else {
+    Console.WriteLine("Наибольшее число = " + num_b);
+    Console.WriteLine("Наименьшее число = " + num_a);
+    }
+
+
